fedee33c7337393b35d9f8f5746f9f6d74fdf608
Md. Mehedi Hasan <mehedibarisal1998@gmail.com>
2023-01-02 15:34:13 +0600

    implemented experts minds component without responsive

---
+++ src/app/modules/landing/home/components/components/experts-minds/experts-minds.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experts-minds',
+  templateUrl: './experts-minds.component.html',
+  styleUrls: ['./experts-minds.component.scss']
+})
+export class ExpertsMindsComponent {
+
+}
--- src/app/modules/landing/home/home.module.ts
+++ src/app/modules/landing/home/home.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { SharedModule } from 'app/shared/shared.module';
+import { RouterModule } from '@angular/router';
 import { LandingHomeComponent } from 'app/modules/landing/home/home.component';
 import { landingHomeRoutes } from 'app/modules/landing/home/home.routing';
+import { SharedModule } from 'app/shared/shared.module';
 import { SwiperModule } from 'swiper/angular';
 
+import { ExpertsMindsComponent } from './components/components/experts-minds/experts-minds.component';
 import { PopularCoursesComponent } from './components/components/popular-courses/popular-courses.component';
 import { SlideContentComponent } from './components/components/slide-content/slide-content.component';
 @NgModule({
     declarations: [
         LandingHomeComponent,
         PopularCoursesComponent,
-        SlideContentComponent
+        SlideContentComponent,
+        ExpertsMindsComponent
     ],
     imports     : [
         RouterModule.forChild(landingHomeRoutes),
