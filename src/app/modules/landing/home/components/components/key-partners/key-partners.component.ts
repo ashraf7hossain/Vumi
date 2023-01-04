c52c7e46294b95f82dcf76bfe2b2fbf06a7ff150
Md. Mehedi Hasan <mehedibarisal1998@gmail.com>
2023-01-04 19:11:34 +0600

    added swiper button outside the swiper div

--- src/app/modules/landing/home/components/components/key-partners/key-partners.component.ts
+++ src/app/modules/landing/home/components/components/key-partners/key-partners.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SwiperComponent } from 'swiper/angular';
+// import Swiper core and required modules
+import SwiperCore, { Virtual } from 'swiper';
 
+// install Swiper modules
+SwiperCore.use([Virtual]);
 @Component({
-  selector: 'app-key-partners',
-  templateUrl: './key-partners.component.html',
-  styleUrls: ['./key-partners.component.scss']
-  // encapsulation: ViewEncapsulation.None
-
+    selector: 'app-key-partners',
+    templateUrl: './key-partners.component.html',
+    styleUrls: ['./key-partners.component.scss'],
+    // encapsulation: ViewEncapsulation.None
 })
 export class KeyPartnersComponent {
-
+    @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
+    slideNext() {
+        this.swiper.swiperRef.slideNext(100);
+    }
+    slidePrev() {
+        this.swiper.swiperRef.slidePrev(100);
+    }
 }
