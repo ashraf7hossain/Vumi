1ba72892fdda0d01f6fedd8a31e32422e07ee820
Md. Mehedi Hasan <mehedibarisal1998@gmail.com>
2023-01-05 16:57:48 +0600

    swipe button fixed on testimonials component without responsive

--- src/app/modules/landing/home/components/components/testimonials/testimonials.component.ts
+++ src/app/modules/landing/home/components/components/testimonials/testimonials.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SwiperComponent } from 'swiper/angular';
+// import Swiper core and required modules
+import SwiperCore, { Virtual } from 'swiper';
 
+// install Swiper modules
+SwiperCore.use([Virtual]);
 @Component({
-  selector: 'app-testimonials',
-  templateUrl: './testimonials.component.html',
-  styleUrls: ['./testimonials.component.scss']
-  // encapsulation: ViewEncapsulation.None
-
+    selector: 'app-testimonials',
+    templateUrl: './testimonials.component.html',
+    styleUrls: ['./testimonials.component.scss'],
+    // encapsulation: ViewEncapsulation.None
 })
 export class TestimonialsComponent {
-
+    @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
+    slidePrevTestimonials() {
+        this.swiper.swiperRef.slidePrev(100);
+    }
+    slideNextTestimonials() {
+        this.swiper.swiperRef.slideNext(100);
+    }
 }
