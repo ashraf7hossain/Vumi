4ba1d1ec79c5e07872e7cbe9a01e70046ca6836a
Md. Mehedi Hasan <mehedibarisal1998@gmail.com>
2023-01-05 14:35:09 +0600

    fixed the swipe button of experts-minds component without responsive

--- src/app/modules/landing/home/components/components/experts-minds/experts-minds.component.ts
+++ src/app/modules/landing/home/components/components/experts-minds/experts-minds.component.ts
@@ -1,11 +1,22 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { SwiperComponent } from 'swiper/angular';
+// import Swiper core and required modules
+import SwiperCore, { Virtual } from 'swiper';
 
+// install Swiper modules
+SwiperCore.use([Virtual]);
 @Component({
-  selector: 'app-experts-minds',
-  templateUrl: './experts-minds.component.html',
-  styleUrls: ['./experts-minds.component.scss'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-experts-minds',
+    templateUrl: './experts-minds.component.html',
+    styleUrls: ['./experts-minds.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class ExpertsMindsComponent {
-
+    @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
+    slideNextExperts() {
+        this.swiper.swiperRef.slideNext(100);
+    }
+    slidePrevExperts() {
+        this.swiper.swiperRef.slidePrev(100);
+    }
 }
