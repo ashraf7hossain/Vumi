c9a25e253958dc102e136d091899db94e1f7656d
Md. Mehedi Hasan <mehedibarisal1998@gmail.com>
2023-01-08 14:12:27 +0600

    responsive testimonials component

--- src/app/modules/landing/home/components/components/testimonials/testimonials.component.ts
+++ src/app/modules/landing/home/components/components/testimonials/testimonials.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
 import { SwiperComponent } from 'swiper/angular';
 // import Swiper core and required modules
 import SwiperCore, { Virtual } from 'swiper';
@@ -9,14 +9,22 @@ SwiperCore.use([Virtual]);
     selector: 'app-testimonials',
     templateUrl: './testimonials.component.html',
     styleUrls: ['./testimonials.component.scss'],
-    // encapsulation: ViewEncapsulation.None
+    encapsulation: ViewEncapsulation.None,
 })
 export class TestimonialsComponent {
     @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
+    @ViewChild('swiper2', { static: false }) swiper2?: SwiperComponent;
+
     slidePrevTestimonials() {
         this.swiper.swiperRef.slidePrev(100);
     }
+    slideNextExpertsPhone() {
+        this.swiper2.swiperRef.slideNext(100);
+    }
     slideNextTestimonials() {
         this.swiper.swiperRef.slideNext(100);
+    }
+    slidePrevExpertsPhone() {
+        this.swiper2.swiperRef.slidePrev(100);
     }
 }
